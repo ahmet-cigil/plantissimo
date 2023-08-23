b8cb61a59c456b809cfb53434ea3030e4d1cdfc1
Ahmet Cigil <ahmetcgln@gmail.com>
2023-08-23 15:06:06 +0200

    layout and router created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
-	return (
-		<div className="App">
-			<h1>Hi</h1>
-		</div>
-	);
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Navbar, Feed, FeedDetail, SearchFeed } from "./components";
+
+const App = () => (
+	<Router>
+		<Box>
+			<Navbar />
+			<Routes>
+				<Route path="/" exact element={<Feed />} />
+				<Route path="/feed-detail/:id" exact element={<FeedDetail />} />
+				<Route path="/search-feed/:searchTerm" exact element={<SearchFeed />} />
+			</Routes>
+		</Box>
+	</Router>
+);
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./Navbar";
+export { default as Feed } from "./Feed";
+export { default as FeedDetail } from "./FeedDetail";
+export { default as SearchFeed } from "./SearchFeed";
